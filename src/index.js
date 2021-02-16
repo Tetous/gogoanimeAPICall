3f6d1e482625702ddfe6584a6a4e8a05fefe9514
PokemonLeader <PokemonLeader6@gmail.com>
2021-02-16 11:14:31 +0300

    uptime counter

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ const { authorization } = require('./function');
 
 const PORT = process.env.PORT || 3000;
 
+app.use('/uptime', (req, res) => {
+    let time = process.uptime();
+    let uptime = (time + "").toHHMMSS();
+    res.send({
+        uptime
+    });
+})
+
 app.use('/api', authorization, router);
 
 const listener = app.listen(PORT, () => {
@@ -27,3 +35,16 @@ const listener = app.listen(PORT, () => {
 //         fetch()
 //     }, 10000);
 // }
+
+String.prototype.toHHMMSS = function () {
+    let sec_num = parseInt(this, 10); // don't forget the second param
+    let hours   = Math.floor(sec_num / 3600);
+    let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    let seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    let time    = hours+':'+minutes+':'+seconds;
+    return time;
+}
